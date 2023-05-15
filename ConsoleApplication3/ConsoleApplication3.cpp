66c8d95875efc473f863dd78aca039b95166f5f8
shabit24 <hossain.shabit24@gmail.com>
2023-05-15 20:02:14 +0700

    create constructor

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int); //constructor
+	angka(); //destructor
+	void cetakData();
+	void isiData();
+};
+//definidi member function
+angka::angka(int i) {
+	panjang = i; //constructor
+	arr = new int[i];
+	isiData();
+}
